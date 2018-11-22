255c5e16eb7648790922089247b746f62b2a6b1a
20171105121 <s20171105121@mac24.local>
2018-11-22 10:02:56 +0800

    1122

--- twelve/ViewController.swift
+++ twelve/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    var offset = 2008 % 12
+   
+   
+    @IBOutlet weak var yearOfBirth: UITextField!
+    @IBOutlet weak var image: UIImageView!
+    
+override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        yearOfBirth.resignFirstResponder()
+        
+    }
+    @IBAction func okTapped(_ sender: Any) {
+        yearOfBirth.resignFirstResponder()
+        if let year = yearOfBirth.text(){
+            var imageNumber = (year - offset) % 12
+            image.image = UIImage(named: String(imageNumber))
+        }
+        else {
+    }
+    
 }
-
+}
